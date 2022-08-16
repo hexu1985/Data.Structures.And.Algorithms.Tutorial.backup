6856f4912161d130ee0b2eac275f1bec9e1e897a
hexu <hexu_1985@sina.com>
2022-08-16 19:00:56 +0800

    update singly_linked_list/recipe-01/cxx/main.cpp

--- singly_linked_list/recipe-01/cxx/main.cpp
+++ singly_linked_list/recipe-01/cxx/main.cpp
@@ -1,6 +1,19 @@
 #include "singly_linked_list.hpp"
+#include <string>
+#include <iostream>
 
 int main()
 {
+    SinglyLinkedList<std::string> words;
+    words.Append("foo");
+    words.Append("bar");
+    words.Append("bim");
+    words.Append("baz");
+    words.Append("quux");
+
+    std::cout << "access by index" << "\n";
+    std::cout << "here is a node: " << words[1] << "\n";
+
+    return 0;
 }
 
