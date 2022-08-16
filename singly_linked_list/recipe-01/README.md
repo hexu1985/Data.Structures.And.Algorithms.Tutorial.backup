254cb3e25c3d7f3cdd2a0cb7e65a034e1a81ec23
hexu <hexu_1985@sina.com>
2022-08-16 17:29:12 +0800

    update python/singly_linked_list.py

--- singly_linked_list/recipe-01/README.md
+++ singly_linked_list/recipe-01/README.md
@@ -1,1 +1,2 @@
 参考代码: Python数据结构和算法实战(第2版)
+singly_linked_list.py
--- singly_linked_list/recipe-01/python/singly_linked_list.py
+++ singly_linked_list/recipe-01/python/singly_linked_list.py
@@ -8,24 +8,24 @@ class SinglyLinkedList:
     """ A singly-linked list. """
     def __init__(self):
         """ Create an empty list. """
-        self.tail = None
         self.head = None
+        self.tail = None
         self.count = 0
 
     def Append(self, data):
         """ Append an item to the list """
         node = Node(data)
-        if self.head:
-            self.head.next = node
-            self.head = node
-        else:
+        if self.tail:
+            self.tail.next = node
             self.tail = node
+        else:
             self.head = node
+            self.tail = node
         self.count += 1
     
     def iter(self):
         """ Iterate through the list. """
-        current = self.tail
+        current = self.head
         while current:
             val = current.data
             current = current.next
@@ -33,12 +33,12 @@ class SinglyLinkedList:
 
     def Delete(self, data):
         """ Delete a node from the list """
-        current = self.tail
-        prev = self.tail
+        current = self.head
+        prev = self.head
         while current:
             if current.data == data:
-                if current == self.tail:
-                    self.tail = current.next
+                if current == self.head:
+                    self.head = current.next
                 else:
                     prev.next = current.next
                 self.count -= 1
@@ -57,7 +57,7 @@ class SinglyLinkedList:
     def __getitem__(self, index):
         if index > self.count - 1:
             raise Exception("Index out of range.")
-        current = self.tail
+        current = self.head
         for n in range(index):
             current = current.next
         return current.data
@@ -65,7 +65,7 @@ class SinglyLinkedList:
     def __setitem__(self, index, value):
         if index > self.count - 1:
             raise Exception("Index out of range.")
-        current = self.tail
+        current = self.head
         for n in range(index):
             current = current.next
         current.data = value
