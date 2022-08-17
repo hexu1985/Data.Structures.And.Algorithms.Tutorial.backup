c9390167a762e2f763b492c5ce1288979f8b0e0a
hexu <hexu_1985@sina.com>
2022-08-17 19:00:57 +0800

    update singly_linked_list.h

--- singly_linked_list/recipe-01/c/singly_linked_list.h
+++ singly_linked_list/recipe-01/c/singly_linked_list.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 // """ A singly-linked node. """
 struct Node {
@@ -11,9 +13,7 @@ struct Node {
 // provide by user
 Node* NewNode(ItemType);
 void FreeNode(Node*);
-
 bool IsEqualItem(ItemType, ItemType);
-ItemType CopyItem(ItemType);
 
 // """ A singly-linked list. """
 struct SinglyLinkedList {
@@ -23,17 +23,46 @@ struct SinglyLinkedList {
 };
 
 // """ Create an empty list. """
-SinglyLinkedList* NewList();
-void FreeList(SinglyLinkedList*);
+static SinglyLinkedList* NewList() {
+    SinglyLinkedList* list = malloc(sizeof(struct SinglyLinkedList));
+    list->head = NULL;
+    list->tail = NULL;
+    list->count = 0;
+}
+
+void FreeList(SinglyLinkedList* list) {
+    Node* current = list->head;
+    while (current) {
+        Node* node = current;
+        current = current->next;
+        FreeNode(node);
+    }
+}
 
 // """ Iterate through the list. """
-void Travel(SinglyLinkedList*, void (*travel)(ItemType));
+void Travel(SinglyLinkedList* list, void (*travel)(ItemType)) {
+    Node* current = list->head;
+    while (current) {
+        travel(current->data);
+        current = current->next;
+    }
+}
 
 // """ Append an item to the list """
-void Append(SinglyLinkedList*, ItemType);
+void Append(SinglyLinkedList* list, ItemType data) {
+    Node* node = NewNode(data);
+    if (list->tail) {
+        list->tail->next = node;
+        list->tail = node;
+    } else {
+        list->head = node;
+        list->tail = node;
+    }
+    list->count += 1;
+}
 
 // """ Delete a node from the list """
-void Delete(SinglyLinkedList*, ItemType);
+void Delete(SinglyLinkedList* list, ItemType);
 
 // """ Search through the list. Return True if data is found, otherwise
 // False. """
@@ -41,3 +70,4 @@ bool Search(SinglyLinkedList*, ItemType);
 
 ItemType GetItem(SinglyLinkedList*, int index);
 void SetItem(SinglyLinkedList*, int index, ItemType);
+
