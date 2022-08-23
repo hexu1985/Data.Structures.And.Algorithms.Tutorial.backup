10c987d3a8480fe3e914e83164b36c5aacf6426a
hexu <hexu_1985@sina.com>
2022-08-23 11:40:09 +0800

    update singly_linked_list/recipe-01/python

--- singly_linked_list/recipe-01/python/singly_linked_list.py
+++ singly_linked_list/recipe-01/python/singly_linked_list.py
@@ -23,7 +23,7 @@ class SinglyLinkedList:
             self.tail = node
         self.count += 1
     
-    def iter(self):
+    def __iter__(self):
         """ Iterate through the list. """
         current = self.head
         while current:
@@ -62,9 +62,12 @@ class SinglyLinkedList:
     def Search(self, data):
         """ Search through the list. Return True if data is found, otherwise
         False. """
-        for node in self.iter():
-            if data == node:
+        current = self.head
+        while current:
+            if data == current.data:
                 return True
+            current = current.next 
+
         return False
 
     def __getitem__(self, index):
@@ -101,7 +104,7 @@ def test():
     print("Modified node by index: {}".format(words[4]))
     
     print("This list has {} elements.".format(words.count))
-    for word in words.iter():
+    for word in words:
         print("Got this data: {}".format(word))
     
     if words.Search('foo'):
@@ -112,19 +115,19 @@ def test():
     print("Now we try to Delete an item")
     words.Delete('bim')
     print("List now has {} elements".format(words.count))
-    for word in words.iter():
+    for word in words:
         print("data: {}".format(word))
     
     print("Delete the first item in the list")
     words.Delete('foo')
     print("size: {}".format(words.count))
-    for word in words.iter():
+    for word in words:
         print("data: {}".format(word))
     
     print("Delete the last item in the list")
     words.Delete('quux')
     print("size: {}".format(words.count))
-    for word in words.iter():
+    for word in words:
         print("data: {}".format(word))
 
 def test_reverse():
@@ -138,12 +141,12 @@ def test_reverse():
     words.Append('quux')
     
     print("The origin list");
-    for word in words.iter():
+    for word in words:
         print("data: {}".format(word))
 
     print("Reverse the list");
     words.Reverse();
-    for word in words.iter():
+    for word in words:
         print("data: {}".format(word))
 
 if __name__ == "__main__":
