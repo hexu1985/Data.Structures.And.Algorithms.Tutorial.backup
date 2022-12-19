3f49fc8667eed44dbefdb2cecd7e88f933cc7afe
hexu <hexu_1985@sina.com>
2022-12-19 17:35:48 +0800

    add test shell for dijkstra

---
+++ dijkstra/recipe-02/cxx/test.sh
@@ -0,0 +1,4 @@
+
+rm -f result.log
+./test_dijkstra >> result.log
+./test_dijkstra2 >> result.log
---
+++ dijkstra/recipe-02/python/test.sh
@@ -0,0 +1,4 @@
+
+rm -f result.log
+python3 -m test_dijkstra >> result.log
+python3 -m test_dijkstra2 >> result.log
